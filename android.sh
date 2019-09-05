a6b1c86a446f6eb84c2b22e5886f1e61d81f6a0d
Maxim Fedorenko <m.fedorenko@corp.mail.ru>
2019-09-05 13:30:06 +0300

    Add build editor phase into the android build script

--- android.sh
+++ android.sh
@@ -6,6 +6,9 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 . "$DIR/common/config.source"
 
+# Build editor
+"$DIR/build.sh" $1
+
 MODE="Development"
 
 PLATFORM="Android"
@@ -13,16 +16,20 @@ PLATFORM="Android"
 echo MODE: $MODE
 echo PLATFORM: $PLATFORM
 
+DIST="$PROJECTS/$1/ArchivedBuilds/Android"
+mkdir -p $DIST
+
 CAPITAL=$(capital "$1")
 UPROJECT="$CAPITAL.uproject"
 
 "$UE4/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun \
     -project="$PROJECTS/$1/$UPROJECT" \
     -nop4 \
-    -clientconfig="$MODE" -utf8output -platform="$PLATFORM" \
+    -clientconfig="$MODE" -nocompileeditor \
+    -utf8output -platform="$PLATFORM" \
     -targetplatform="$PLATFORM" -cookflavor="ASTC" \
     -build -cook -messaging \
     -unversionedcookedcontent -compressed -stage -package \
-    -prereqs -distribution \
+    -prereqs -distribution -archive -archivedirectory="$DIST"\
     "${@:2}"
 
