9f2f62651fd3aa288737e50ebeb018512381e3cb
Maxim Fedorenko <m.fedorenko@corp.mail.ru>
2019-09-05 17:20:30 +0300

    Fixup clean script

--- clean.sh
+++ clean.sh
@@ -9,8 +9,8 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 rm -rfv "$PROJECTS/$1/Binaries"
 rm -rfv "$PROJECTS/$1/Intermediate"
 
-rm -rfv "$PROJECTS/$1/Plugins/*/Binaries"
-rm -rfv "$PROJECTS/$1/Plugins/*/Intermediate"
+rm -rfv "$PROJECTS/$1/Plugins/"*"/Binaries"
+rm -rfv "$PROJECTS/$1/Plugins/"*"/Intermediate"
 
 #find ./Plugins/ -type d -name 'Intermediate' -or -name 'Binaries' |while read x; do rm -rfv "$x"; done
 
